e0f28a5528e62532daca943a9d63a3cdddd84d14
olgabiro <o.rosik.rosinska@gmail.com>
2023-03-09 21:18:47 +0100

    Implemented the quiz builder page.

--- src/main.ts
+++ src/main.ts
@@ -84,3 +84,53 @@ function answerQuestion(answerCode: AnswerCode) {
     }
     nextQuestion();
 }
+
+function saveQuiz() {
+    quiz.clear()
+    Array.from(document.getElementsByClassName("question")).map(it => createQuestion(it)).forEach(addQuestion);
+    window.location.href = "./quiz.html"
+}
+
+function getInputValue(inputElement: Element) {
+    return (inputElement as HTMLInputElement).value!;
+}
+
+function createQuestion(question: Element): Question {
+    console.log(question)
+    let description = getInputValue(question.getElementsByClassName("question-text")
+        .item(0)!);
+    let answerTexts = Array.from(question.getElementsByClassName("answer-text"))
+        .map(answer => getInputValue(answer))
+    console.log(answerTexts)
+    let answers = [new Answer(AnswerCode.A, answerTexts[0]),
+        new Answer(AnswerCode.B, answerTexts[1]),
+        new Answer(AnswerCode.C, answerTexts[2]),
+        new Answer(AnswerCode.D, answerTexts[3])]
+
+    return new Question(description, findCorrectAnswer(question), answers);
+}
+
+function findCorrectAnswer(question: Element): AnswerCode {
+    let checkedName = Array.from(question.getElementsByTagName("input")).filter(it => it.type == "checkbox").filter(it => it.checked)[0]
+
+    switch (checkedName.name) {
+        case "correct-a":
+            return AnswerCode.A;
+        case "correct-b":
+            return AnswerCode.B
+        case "correct-c":
+            return AnswerCode.C
+        default:
+            return AnswerCode.D;
+    }
+}
+
+function displayNewQuestion() {
+    let quiz = document.getElementById("questions")!;
+    let template = document.getElementsByTagName("template")[0];
+    let newQuestionIndex = quiz.childElementCount + 1
+    console.log(newQuestionIndex)
+    let question = template.content.cloneNode(true);
+    quiz.append(question)
+    quiz.children.item(quiz.childElementCount - 1)!.getElementsByTagName("span")[0]!.textContent = newQuestionIndex.toString()
+}
